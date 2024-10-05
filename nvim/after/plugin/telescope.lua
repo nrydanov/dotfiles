1524ae7b80db17061e9d7f3839d23b37e2799b55
Nikita Rydanov <find.art.in.living@gmail.com>
2024-10-05 11:29:49 +0400

    feat: Changed telescope settings

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,2 +1,25 @@
 require('telescope').setup {
+    defaults = {
+        layout_strategy = 'vertical',
+        layout_config = {
+            preview_cutoff = 40,
+            horizontal = { preview_width = 0.5 },
+            vertical = { width=0.7 }
+            -- other layout configuration here
+        },
+        preview = {
+            wrap = true,
+        },
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "--with-filename",
+            "--line-number",
+            "--column",
+            "--smart-case",
+            "--trim" -- add this value
+        }
+        -- other defaults configuration here
+    },
 }
