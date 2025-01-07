03937fc84c319e9300fd7507e31b9185f9d58974
Nikita Rydanov <find.art.in.living@gmail.com>
2025-01-07 16:59:45 +0400

    feat: Added venv alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,6 +3,8 @@ alias vim=nvim
 ## Kubernetes
 alias k="kubectl"
 
+alias venv="source .venv/bin/activate"
+
 # MacOS specific aliases
 if [ "$(uname)" = "Darwin" ]; then
     # alias ssh="kitten ssh"
