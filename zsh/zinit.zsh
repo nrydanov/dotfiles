d68cca0cd87e074590183a96bb296ddf5fca4823
Nikita Rydanov <find.art.in.living@gmail.com>
2025-01-07 16:59:18 +0400

    fix: Fixed wrong order of plugin loading for Linux systems

--- zsh/zinit.zsh
+++ zsh/zinit.zsh
@@ -19,6 +19,6 @@ zinit light-mode for \
     zdharma-continuum/zinit-annex-bin-gem-node \
     zdharma-continuum/zinit-annex-patch-dl \
     zdharma-continuum/zinit-annex-rust \
+    zsh-users/zsh-syntax-highlighting \
     marlonrichert/zsh-autocomplete \
-    zsh-users/zsh-completions \
-    zsh-users/zsh-syntax-highlighting
+    zsh-users/zsh-completions
