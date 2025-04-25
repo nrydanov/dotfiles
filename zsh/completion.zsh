23895afda63e80e7dca56cf26ddba00ba8da823d
Nikita Rydanov <find.art.in.living@gmail.com>
2025-04-25 21:46:19 +0400

    fix: Fixed autocompletion lags

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,17 +4,9 @@ LISTMAX=-1
 zstyle ':completion:*' use-cache on
 zstyle ':completion:*' cache-path "$XDG_CACHE_HOME/zsh/.zcompcache"
 
-# Configure completion display and menu selection
-zstyle ':completion:*' menu select
-zstyle ':completion:*:descriptions' format '%F{green}-- %d --%f'
-zstyle ':completion:*:corrections' format '%F{yellow}!- %d (errors: %e) -!%f'
-zstyle ':completion:*:messages' format '%F{purple}-- %d --%f'
-zstyle ':completion:*:warnings' format '%F{red}-- no matches found --%f'
 zstyle ':completion:*:default' list-colors "${LS_COLORS}"
 zstyle ':autocomplete:*' min-delay 1.0  # float
 zstyle ':autocomplete:*' min-input 3
-zstyle ':completion:*' list-prompt   ''
-zstyle ':completion:*' select-prompt ''
 
 
 bindkey              '^I' menu-select
@@ -32,7 +24,6 @@ HISTSIZE=9999
 
 setopt SHARE_HISTORY HIST_EXPIRE_DUPS_FIRST PROMPT_SUBST HIST_IGNORE_ALL_DUPS
 unsetopt pathdirs
-
 
 # Zsh autosuggestions configuration (if using the plugin)
 export ZSH_AUTOSUGGEST_STRATEGY=(completion history)
