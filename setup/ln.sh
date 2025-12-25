e8dbcd414bcc490c7d3018ff1841e6e13e005058
Nikita Rydanov <find.art.in.living@gmail.com>
2025-12-25 16:32:20 +0400

    fix: Fixed typo in ln setup script

--- setup/ln.sh
+++ setup/ln.sh
@@ -1,6 +1,6 @@
 ln -s $XDG_CONFIG_HOME/dotfiles/.zshrc ~/.zshrc
 ln -s $XDG_CONFIG_HOME/dotfiles/.zshenv ~/.zshenv
-ln -s $XDG_CONFIG/HOME/dotfiles/nvim $XDG_CONFIG_HOME/nvim
+ln -s $XDG_CONFIG_HOME/dotfiles/nvim $XDG_CONFIG_HOME/nvim
 ln -s $XDG_CONFIG_HOME/dotfiles/kitty $XDG_CONFIG_HOME/kitty
 ln -s $XDG_CONFIG_HOME/dotfiles/.zprofile $XDG_CONFIG_HOME/.zprofile
 ln -s $XDG_CONFIG_HOME/dotfiles/zellij $XDG_CONFIG_HOME/zellij
