c54a6f82b0a9a23fed9318d3cd96ea79cf12980d
Nikita Rydanov <find.art.in.living@gmail.com>
2025-12-25 13:25:41 +0400

    chore: Some recoloring

--- nvim/lua/plugins/onedarkpro.lua
+++ nvim/lua/plugins/onedarkpro.lua
@@ -1,9 +1,32 @@
 return {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
-    opts = {
-        options = {
-            transparency = true, -- Use a transparent background?
+    opts = function()
+        local helper = require("onedarkpro.helpers")
+        local colors = helper.get_colors() -- берёт палитру текущей темы
+
+        return {
+            options = {
+                transparency = true, -- Use a transparent background?
+            },
+            highlights = {
+                CursorLineNr = {
+                    fg = colors.blue
+                },
+                NvimTreeFolderIcon = {
+                    fg = colors.blue
+                },
+                NvimTreeRootFolder = {
+                    fg = colors.green
+                },
+                NvimTreeOpenedFolderName = {
+                    fg = colors.green
+                },
+                ["@lsp.type.enumMember"] = {
+                    fg = colors.green
+                }
+            }
+
         }
-    }
+    end
 }
