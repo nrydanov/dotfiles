c2324898eef586f7a4b4e08af478b79f50b7210a
Nikita Rydanov <find.art.in.living@gmail.com>
2026-04-14 10:53:15 +0400

    feat: better geosite data download script

--- maintainance/update_xray_lists.sh
+++ maintainance/update_xray_lists.sh
@@ -1,2 +1,17 @@
-wget -O /Applications/V2RayXS.app/Contents/Resources/geosite.dat https://github.com/itdoginfo/allow-domains/releases/latest/download/geosite.dat
+#!/bin/sh
 
+GEOSITE_URL="https://raw.githubusercontent.com/runetfreedom/russia-v2ray-rules-dat/release/geosite.dat"
+GEOIP_URL="https://raw.githubusercontent.com/runetfreedom/russia-v2ray-rules-dat/release/geoip.dat"
+
+GEOSITE_CLIENT_PATH="/Applications/V2RayXS.app/Contents/Resources/geosite.dat"
+GEOIP_CLIENT_PATH="/Applications/V2RayXS.app/Contents/Resources/geoip.dat"
+
+SYMLINK_DIR="$HOME/.geodata"
+
+mkdir -p "$SYMLINK_DIR"
+
+ln -sf "$GEOSITE_CLIENT_PATH" "$SYMLINK_DIR/geosite.dat"
+ln -sf "$GEOIP_CLIENT_PATH" "$SYMLINK_DIR/geoip.dat"
+
+curl -fsSL -o "$SYMLINK_DIR/geosite.dat" "$GEOSITE_URL"
+curl -fsSL -o "$SYMLINK_DIR/geoip.dat" "$GEOIP_URL"
