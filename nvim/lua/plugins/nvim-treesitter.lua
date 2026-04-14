ac9fecb78b8381a1fbfbd716e0769530f0bda69f
Nikita Rydanov <find.art.in.living@gmail.com>
2026-04-14 10:35:48 +0400

    fix: ts fix

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,13 +1,11 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    version = "*",
+    branch = "*",
+    lazy = false,
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup {
-            ensure_installed = { "rust", "lua", "python" },
-            highlight = { enable = true, disable = { 'html' } },
-            indent = { enable = true },
+        require("nvim-treesitter").setup({
             auto_install = true,
-        }
+        })
     end
 }
