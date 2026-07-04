a75e9408010356710904918d1921b7f638e925a3
Nikita Rydanov <find.art.in.living@gmail.com>
2026-07-04 21:54:45 +0400

    fix: bind Option+Left/Right word-jump for Ghostty's meta encoding

--- zsh/bindkeys.zsh
+++ zsh/bindkeys.zsh
@@ -1,7 +1,9 @@
 # Soeviy terminal
 bindkey '^[[A' up-line-or-search
 bindkey '^[[B' down-line-or-search
-bindkey '^[[1;3D' backward-word  # alt-left
-bindkey '^[[1;3C' forward-word   # alt-right
+bindkey '^[[1;3D' backward-word  # alt-left (kitty CSI encoding)
+bindkey '^[[1;3C' forward-word   # alt-right (kitty CSI encoding)
+bindkey '^[b' backward-word      # alt-left (Ghostty/Terminal.app meta encoding)
+bindkey '^[f' forward-word       # alt-right (Ghostty/Terminal.app meta encoding)
 bindkey '^A' beginning-of-line
 bindkey '^E' end-of-line
